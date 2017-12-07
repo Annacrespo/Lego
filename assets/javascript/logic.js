58a9da3cc8777150da8772fe24cd0af3b206664c
annacrespo12 <annacrespo12@gmail.com>
2017-12-06 21:35:22 -0800

    reordered images

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -14,3 +14,8 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+less = {
+    env: "development"
+  };
